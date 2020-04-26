a943566e84bc0f6ad6afcc4823784a541d2d740e
Tricky-T <63791476+Tricky-T@users.noreply.github.com>
2020-04-26 11:02:32 -0400

    BIG Update

--- src/NextPlaylist.java
+++ src/NextPlaylist.java
@@ -124,13 +124,19 @@ public class NextPlaylist extends Application
   }//end of printSongs
   
   
-  private TextField searchbar1, searchbar2, FirstName, LastName, searchbar5, searchbar6;
-  private Label label, resultLabel4, labeltitle, labeltitleS, songname2, resultTitle;
+  private TextField searchbar1, searchbar3, FirstName, LastName, searchbar5, searchbar6;
+  private Label label, resultLabel4, labeltitle, labeltitleS, songname2, resultTitle, resultHistory, resultArtist, resultYear;
   private RadioButton ShortButton, MediumButton, LongButton;
   private CheckBox g1,g2,g3,g4,g5,g6,g7,g8,g9;
-  public String search;
+  private String search, Titletext, Albumtext, Yeartext;
+  private String searchTitle, searchArtist, searchAlbum, searchGenre, searchLength, searchYear;
+  private Button toGenre, toTitle, toArtist, toLength, toAlbum, toYear, toHistory0, toHistory1, toHistory2, toHistory3, toHistory4,toHistory5, toHistory6;
+  private Button backToMain1, backToMain2, backToMain3, backToMain4, backToMain5, backToMain6, backToMain7;
+  private Button search1, search2, search3, search4, search5, search6;
+  private VBox S0Main, S4Genre, S1Title, S2Artist, S5Length, S3Album, S6Year, S7History;
+  private HBox buttonOp1,buttonOp2,buttonOp3,buttonOp4,buttonOp5,buttonOp6;
   Stage window;
-  Scene scene0,scene1,scene2, scene2x1, scene3, scene4, scene5, scene6;
+  Scene scene0,scene1,scene2, scene3, scene4, scene5, scene6, scene7;
   
   @Override
   public void start(Stage primaryStage) throws Exception
@@ -141,31 +147,162 @@ public class NextPlaylist extends Application
     window = primaryStage;
     //scene0
     Label label0 = new Label("Click on the what do want to search by");
-    Button label1 = new Button(" Genre ");
-    Button label2 = new Button(" Title ");
-    Button label3 = new Button(" Artist ");
-    Button label4 = new Button(" Length ");
-    Button label5 = new Button(" Album ");
-    Button label6 = new Button(" Year ");
-    label = new Label();
+
+    toTitle = new Button("Title");
+    toTitle.setMaxSize(100, 200);
+    toArtist = new Button("Artist");
+    toArtist.setMaxSize(100, 200);
+    toAlbum = new Button("Album");
+    toAlbum.setMaxSize(100, 200);
+    toGenre = new Button("Genre");
+    toGenre.setMaxSize(100, 200);
+    toLength = new Button("Length");
+    toLength.setMaxSize(100, 200);
+    toYear = new Button("Year");
+    toYear.setMaxSize(100, 200);
+    toHistory0 = new Button("History");
+    toHistory0.setMaxWidth(148);
+    toHistory0.setStyle("-fx-text-fill: #0000ff");
     GridPane gridpane = new GridPane();
-    gridpane.add(label1,0,1);
-    gridpane.add(label2,1,1);
-    gridpane.add(label3,2,1);
-    gridpane.add(label4,0,2);
-    gridpane.add(label5,1,2);
-    gridpane.add(label6,2,2);
-    label1.setOnAction(e -> window.setScene(scene1));
-    label2.setOnAction(e -> window.setScene(scene2));
-    label3.setOnAction(e -> window.setScene(scene3));
-    label4.setOnAction(e -> window.setScene(scene4));
-    label5.setOnAction(e -> window.setScene(scene5));
-    label6.setOnAction(e -> window.setScene(scene6));
-    VBox vbox = new VBox(10, label0, gridpane, label);
-    scene0 = new Scene(vbox, 400, 200);
+    gridpane.add(toTitle,0,1);
+    gridpane.add(toArtist,1,1);
+    gridpane.add(toAlbum,2,1);
+    gridpane.add(toGenre,0,2);
+    gridpane.add(toLength,1,2);
+    gridpane.add(toYear,2,2);
+    toTitle.setOnAction(e -> window.setScene(scene1));
+    toArtist.setOnAction(e -> window.setScene(scene2));
+    toAlbum.setOnAction(e -> window.setScene(scene3));
+    toGenre.setOnAction(e -> window.setScene(scene4));
+    toLength.setOnAction(e -> window.setScene(scene5));
+    toYear.setOnAction(e -> window.setScene(scene6));
+    toHistory0.setOnAction(e -> window.setScene(scene7));
+    S0Main = new VBox(10, label0, gridpane, toHistory0);
+    scene0 = new Scene(S0Main, 300, 200);
+    S0Main.setAlignment(Pos.CENTER);
+    gridpane.setAlignment(Pos.CENTER);
     //end scene 0
     
-    //scene1
+    //scene 1
+     StackPane layoutTitle = new StackPane();
+     Label message1 = new Label("Search By Title");
+     Label sp1 = new Label();
+     searchbar1 = new TextField();
+     searchbar1.setPrefColumnCount(15);
+     resultTitle = new Label();
+     search1 = new Button("Search");
+     backToMain1 = new Button("Back");
+     backToMain1.setOnAction(w -> window.setScene(scene0));
+     toHistory1 = new Button("History");
+     toHistory1.setOnAction(w -> window.setScene(scene7));
+     toHistory1.setMaxWidth(148);
+     toHistory1.setStyle("-fx-text-fill: #0000ff");
+     search1.setOnAction(event->
+     {
+       Titletext = searchbar1.getText();
+       searchTitle = getSongs("SELECT * FROM Song WHERE Song.title = \"" + Titletext + "\"");
+       System.out.println(searchbar1.getText());
+       resultTitle.setText(searchTitle);
+       //add to resultHistory.setText(searchTitle);
+     });//End handle
+     buttonOp1 = new HBox(10, search1, backToMain1);
+     GridPane gridT1 = new GridPane();
+     gridT1.add(message1, 0, 0);
+     gridT1.add(searchbar1, 0, 1);
+     gridT1.add(sp1, 0, 3);
+     gridT1.add(buttonOp1, 0, 4);
+     S1Title = new VBox(10, message1, gridT1, resultTitle, toHistory1);
+     layoutTitle.getChildren().add(S1Title);
+     scene1 = new Scene(layoutTitle, 500, 200);
+     buttonOp1.setAlignment(Pos.CENTER);
+     S1Title.setAlignment(Pos.CENTER);
+     gridT1.setAlignment(Pos.CENTER);
+    //end scene1
+        
+    //scene 2
+     StackPane layoutArtist = new StackPane();
+     Label message2 = new Label("Search By: ");
+     Label labelF = new Label("First Name");
+     Label labelL = new Label("Last Name");
+     Label resultArtist = new Label();
+     FirstName = new TextField();
+     LastName = new TextField();
+     search2 = new Button("Search");
+     search2.setOnAction(event->
+     {
+       String searchF = FirstName.getText();
+       String searchL = LastName.getText();
+       if (searchL.length()==0)
+       searchArtist = getSongs("SELECT * FROM Song WHERE Song.artist = \"" + searchF + "\"");
+       else if (searchF.length()==0)
+       searchArtist = getSongs("SELECT * FROM Song WHERE Song.artist = \"" + searchL + "\"");
+       else
+       searchArtist = getSongs("SELECT * FROM Song WHERE Song.artist = \"" + (searchF + " " + searchL)+ "\"");
+       resultArtist.setText(searchArtist);
+       //add to resultHistory.setText(searchArtist);
+     });
+     backToMain2 = new Button("Back");
+     backToMain2.setOnAction(y -> window.setScene(scene0));
+     toHistory2 = new Button("History");
+     toHistory2.setOnAction(w -> window.setScene(scene7));
+     toHistory2.setMaxWidth(148);
+     toHistory2.setStyle("-fx-text-fill: #0000ff");
+     GridPane ArtistGrid = new GridPane();
+     ArtistGrid.add(message2, 0, 0);
+     ArtistGrid.add(labelF, 0, 1);
+     ArtistGrid.add(labelL, 1, 1);
+     ArtistGrid.add(FirstName, 0, 2);
+     ArtistGrid.add(LastName, 1, 2);
+     buttonOp2 = new HBox(10, search2, backToMain2);
+     S2Artist = new VBox(10, ArtistGrid, buttonOp2, resultArtist, toHistory2);
+     layoutArtist.getChildren().add(S2Artist);
+     scene2 = new Scene(layoutArtist, 500, 200);
+     S2Artist.setAlignment(Pos.CENTER);
+     buttonOp2.setAlignment(Pos.CENTER);
+     ArtistGrid.setAlignment(Pos.CENTER);
+    //end scene2 
+   
+    //scene 3
+    StackPane layoutAlbum = new StackPane();
+    Label message3 = new Label("Search By Album");
+    Label resultAlbum = new Label();
+    Label sp3x1 = new Label();
+    Label sp3x2 = new Label();
+    searchbar3 = new TextField();
+    searchbar3.setPrefColumnCount(15);
+    search3 = new Button("Search");
+    search3.setOnAction(event->
+     {
+       Albumtext = searchbar3.getText();
+       searchAlbum = getSongs("SELECT * FROM Song WHERE Song.title = \"" + Albumtext + "\"");
+       System.out.println(searchbar3.getText());
+       resultAlbum.setText(searchAlbum);
+       //add to resultHistory.setText(searchTitle);
+     });//End handle
+    backToMain3 = new Button("Back");
+    backToMain3.setOnAction(y -> window.setScene(scene0));
+    toHistory3 = new Button("History");
+    toHistory3.setOnAction(w -> window.setScene(scene7));
+    toHistory3.setMaxWidth(148);
+    toHistory3.setStyle("-fx-text-fill: #0000ff");
+    HBox textlock3 = new HBox(sp3x1, searchbar3, sp3x2);
+    buttonOp3 = new HBox(10, search3, backToMain3);
+    
+    GridPane gridT3 = new GridPane();
+    gridT3.add(message3, 0, 0);
+    gridT3.add(searchbar3, 0, 1);
+    gridT3.add(sp3x1, 0, 3);
+    gridT3.add(buttonOp3, 0, 4);
+    
+    S3Album = new VBox(10, gridT3, resultAlbum, toHistory3);
+    layoutAlbum.getChildren().add(S3Album);
+    scene3 = new Scene(layoutAlbum, 500, 200);
+    S3Album.setAlignment(Pos.CENTER);
+    textlock3.setAlignment(Pos.CENTER);
+    buttonOp3.setAlignment(Pos.CENTER);
+    gridT3.setAlignment(Pos.CENTER);
+    //end scene 3
+    //scene4
     StackPane layoutGenre = new StackPane();
     Label labelG = new Label("Check the ones you would like to search by:");
     g1 = new CheckBox("POP");
@@ -187,80 +324,18 @@ public class NextPlaylist extends Application
     Genregrid.add(g7,2,0);
     Genregrid.add(g8,2,1);
     Genregrid.add(g9,2,2);
-    
-    Button search1 = new Button("Search");
-    search1.setOnAction(e -> handleOptions(g1,g2,g3,g4,g5,g6,g7,g8,g9));
-    Button back1 = new Button("Back");
-    back1.setOnAction(w -> window.setScene(scene0));
-    HBox buttonOp1 = new HBox(10, search1, back1);
-    VBox layout1 = new VBox(10, labelG, Genregrid, buttonOp1);
-    layoutGenre.getChildren().add(layout1);
-    scene1 = new Scene(layoutGenre, 400, 200);
-    //end scene1
-    
-//scene 2
-     StackPane layoutTitle = new StackPane();
-     searchbar2 = new TextField();
-     Label found2 = new Label();
-     Button search2 = new Button("Search");
-     //search2.setOnAction(y -> window.setScene(scene2x1));
-     search2.setOnAction(event->
-     {
-       search = searchbar2.getText();
-       printSongs(System.out, "SELECT * FROM Song WHERE Song.title = \"" + search + "\"");
-       System.out.println(searchbar2.getText());
-       resultTitle.setText(search);
-       found2.setText("Found hit result");
-     });//End handle
-     Button result2 = new Button("Result");
-     result2.setOnAction(y -> window.setScene(scene2x1));
-     Button back2 = new Button("Back");
-     back2.setOnAction(y -> window.setScene(scene0));
-     HBox buttonOp2 = new HBox(10, search2, back2);
-     VBox layout2 = new VBox(10, searchbar2, buttonOp2,found2, result2);
-     layoutTitle.getChildren().add(layout2);
-     scene2 = new Scene(layoutTitle, 400, 200);
-    //end scene2
-    
-    //scene2.1
-    StackPane layoutTitleS = new StackPane();
-    labeltitleS = new Label("Found: ");
-    String s = "xxx";
-    resultTitle = new Label();
-    //printSongs(System.out, "SELECT * FROM Song WHERE Song.title = \"" + searchbar2.getText() + "\"");
-    songname2 = new Label(s);
-    Button back2x1 = new Button("Back");
-    back2x1.setOnAction(y -> window.setScene(scene2));
-    HBox tt = new HBox(labeltitleS, resultTitle);
-    VBox layout2x1 = new VBox(10, tt, songname2, back2x1);
-    layoutTitleS.getChildren().add(layout2x1);
-    scene2x1 = new Scene(layoutTitleS, 400, 200);
-    //end scene2.1
-
-    
-    //scene 3
-     StackPane layoutArtist = new StackPane();
-     Label message3 = new Label("Search By: ");
-     Label labelF = new Label("First Name");
-     Label labelL = new Label("Last Name");
-     FirstName = new TextField();
-     LastName = new TextField();
-     Button search3 = new Button("Search");
-     search3.setOnAction(new ButtonClickHandler3());
-     Button back3 = new Button("Back");
-     back3.setOnAction(y -> window.setScene(scene0));
-     GridPane ArtistGrid = new GridPane();
-     ArtistGrid.add(labelF, 0, 0);
-     ArtistGrid.add(labelL, 1, 0);
-     ArtistGrid.add(FirstName, 0, 1);
-     ArtistGrid.add(LastName, 1, 1);
-     HBox buttonOp3 = new HBox(10, search3, back3);
-     VBox layout3 = new VBox(10,message3, ArtistGrid, buttonOp3);
-     layoutArtist.getChildren().add(layout3);
-     scene3 = new Scene(layoutArtist, 400, 200);
-    //end scene3 
-    
-    //scene4
+    backToMain4 = new Button("Back");
+    backToMain4.setOnAction(w -> window.setScene(scene0));
+    search4 = new Button("Search");
+    search4.setOnAction(e -> handleOptions(g1,g2,g3,g4,g5,g6,g7,g8,g9));
+    toHistory4 = new Button("History");
+    toHistory4.setOnAction(w -> window.setScene(scene7));
+    buttonOp4 = new HBox(10, search4, backToMain4);
+    S4Genre = new VBox(10, labelG, Genregrid, buttonOp4, toHistory4);
+    layoutGenre.getChildren().add(S4Genre);
+    scene4 = new Scene(layoutGenre, 500, 200);
+    //end scene4
+    //scene5
     StackPane layoutlength = new StackPane();
     ShortButton = new RadioButton("Short");
     MediumButton = new RadioButton("Medium");
@@ -270,43 +345,79 @@ public class NextPlaylist extends Application
     MediumButton.setToggleGroup(radioGroup);
     LongButton.setToggleGroup(radioGroup);
     MediumButton.setSelected(true);
-    Button search4 = new Button("Search");
-    search4.setOnAction(new ButtonClickHandler4());
-    Button back4 = new Button("Back");
-    back4.setOnAction(w -> window.setScene(scene0));
-    
+    search5 = new Button("Search");
+    search5.setOnAction(new ButtonClickHistoryandler4());
+    backToMain5 = new Button("Back");
+    backToMain5.setOnAction(y -> window.setScene(scene0));
+    toHistory5 = new Button("History");
+    toHistory5.setOnAction(w -> window.setScene(scene7));    
     HBox radioButton = new HBox(20, ShortButton, MediumButton, LongButton);
-    HBox buttonOp4 = new HBox(10, search4, back4);
-    VBox layout4 = new VBox(10, radioButton, buttonOp4);
-    layoutlength.getChildren().add(layout4);
-    scene4 = new Scene(layoutlength,400,200);
-    //end scene4
-    
-    //scene 5
-    StackPane layoutAlbum = new StackPane();
-    searchbar5 = new TextField();
-    Button search5 = new Button("Search");
-    search5.setOnAction(new ButtonClickHandler5());
-    Button back5 = new Button("Back");
-    back5.setOnAction(y -> window.setScene(scene0));
-    HBox buttonOp5 = new HBox(10, search5, back5);
-    VBox layout5 = new VBox(10, searchbar5, buttonOp5);
-    layoutAlbum.getChildren().add(layout5);
-    scene5 = new Scene(layoutAlbum, 400, 200);
-    //end scene 5
-    
+    buttonOp5 = new HBox(10, search5, backToMain5);
+    S5Length = new VBox(10, radioButton, buttonOp5, toHistory5);
+    layoutlength.getChildren().add(S5Length);
+    scene5 = new Scene(layoutlength,500,200);
+    //end scene5
     //scene6
-    StackPane layoutYear = new StackPane();
+  /*  StackPane layoutYear = new StackPane();
     searchbar6 = new TextField();
-    Button search6 = new Button("Search");
-    search6.setOnAction(new ButtonClickHandler6());
-    Button back6 = new Button("Back");
-    back6.setOnAction(y -> window.setScene(scene0));
-    HBox buttonOp6 = new HBox(10, search6, back6);
-    VBox layout6 = new VBox(10, searchbar6, buttonOp6);
-    layoutYear.getChildren().add(layout6);
-    scene6 = new Scene(layoutYear, 400, 200);
+    search6 = new Button("Search");
+    search6.setOnAction(new ButtonClickHistoryandler6());
+    backToMain6 = new Button("Back");
+    backToMain6.setOnAction(y -> window.setScene(scene0));
+    toHistory6 = new Button("History");
+    toHistory6.setOnAction(w -> window.setScene(scene7));
+    buttonOp6 = new HBox(10, search6, backToMain6);
+    S6Year = new VBox(10, searchbar6, buttonOp6, toHistory6);
+    layoutYear.getChildren().add(S6Year);
+    scene6 = new Scene(layoutYear, 500, 200);
+    */
+    StackPane layoutYear = new StackPane();
+    Label message6 = new Label("Search By Title");
+    Label sp6 = new Label();
+    searchbar6 = new TextField();
+    searchbar6.setPrefColumnCount(15);
+    resultYear = new Label();
+    search6 = new Button("Search");
+    backToMain6 = new Button("Back");
+    backToMain6.setOnAction(w -> window.setScene(scene0));
+    toHistory6 = new Button("History");
+    toHistory6.setOnAction(w -> window.setScene(scene7));
+    toHistory6.setMaxWidth(148);
+    toHistory6.setStyle("-fx-text-fill: #0000ff");
+    search6.setOnAction(event->
+     {
+       Yeartext = searchbar6.getText();
+       searchYear = getSongs("SELECT * FROM Song WHERE Song.releaseyear = \"" + Yeartext + "\"");
+       System.out.println(searchbar6.getText());
+       resultYear.setText(searchYear);
+       //add to resultHistory.setText(searchTitle);
+     });//End handle
+    buttonOp6 = new HBox(10, search6, backToMain6);
+    GridPane gridT6 = new GridPane();
+    gridT6.add(message6, 0, 0);
+    gridT6.add(searchbar6, 0, 1);
+    gridT6.add(sp6, 0, 3);
+    gridT6.add(buttonOp6, 0, 4);
+    S6Year = new VBox(10, message6, gridT6, resultYear, toHistory6);
+    layoutYear.getChildren().add(S6Year);
+    scene6 = new Scene(layoutYear, 550, 400);
+    buttonOp6.setAlignment(Pos.CENTER);
+    S6Year.setAlignment(Pos.CENTER);
+    gridT6.setAlignment(Pos.CENTER);
     //end scene6
+    
+    //scene7 add the array here so it can keep all the search made
+    StackPane layoutHistory = new StackPane();
+    Label xs = new Label("History: ");
+    resultHistory = new Label();
+    backToMain7 = new Button("Back");
+    backToMain7.setOnAction(y -> window.setScene(scene0));
+    S7History = new VBox(10, xs, resultHistory, backToMain7);
+    layoutHistory.getChildren().add(S7History);
+    scene7 = new Scene(layoutHistory, 500, 200);
+    //end scene7
+
+
     
     //Start Scene
     primaryStage.setScene(scene0);
@@ -340,31 +451,8 @@ private void handleOptions(CheckBox  g1, CheckBox  g2, CheckBox  g3, CheckBox  g
       message += "Instrumental\n";
       System.out.println(message);
   }//End handleOptions
-class ButtonClickHandler2 implements EventHandler<ActionEvent> 
-  {
-    @Override
-    public void handle(ActionEvent event)
-    {
-      String search = searchbar2.getText();
-      printSongs(System.out, "SELECT * FROM Song WHERE Song.title = \"" + search + "\"");
-      System.out.println(searchbar2.getText());
-    }//End handle
-  }//End ButtomClickHandler
-class ButtonClickHandler3 implements EventHandler<ActionEvent>
-  {
-    @Override
-    public void handle(ActionEvent event)
-    {
-      //String 
-      String searchF = FirstName.getText();
-      String searchL = LastName.getText();
-      // NOTE Artist search should just be one variable
-      printSongs(System.out, "SELECT * FROM Song WHERE Song.artist = \"" + searchF + "\"");
-      System.out.println(searchF + " " + searchL);
-    }//End handle
-  }//End ButtomClickHandler
 
-class ButtonClickHandler4 implements EventHandler<ActionEvent>
+class ButtonClickHistoryandler4 implements EventHandler<ActionEvent>
   {
     @Override
     public void handle(ActionEvent event)
@@ -385,27 +473,7 @@ class ButtonClickHandler4 implements EventHandler<ActionEvent>
       }
       
     }//End handle
-  }//End ButtomClickHandler
-class ButtonClickHandler5 implements EventHandler<ActionEvent>
-  {
-    @Override
-    public void handle(ActionEvent event)
-    {
-      String search = searchbar5.getText();
-      printSongs(System.out, "SELECT * FROM Song WHERE Song.album = \"" + search + "\"");
-      System.out.println(search);
-    }//End handle
-  }//End ButtomClickHandler
-class ButtonClickHandler6 implements EventHandler<ActionEvent>
-  {
-    @Override
-    public void handle(ActionEvent event)
-    {
-      int search = Integer.parseInt(searchbar6.getText());
-      printSongs(System.out, "SELECT * FROM Song WHERE Song.releaseyear = \"" + search + "\"");
-      System.out.println(search);
-    }//End handle
-  }//End ButtomClickHandler
+  }//End ButtomClickHistoryandler
 
   public static void main(String[] args)
   {
